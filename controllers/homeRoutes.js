7ae55c9b467aad9e73c11f5e4a4b43d714227f6d
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-10-05 22:57:17 -0400

    fixed the bug in the homeroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['username'],
         },
       ],
     });
@@ -40,7 +40,7 @@ router.get('/blog/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['username'],
         },
       ],
     });
