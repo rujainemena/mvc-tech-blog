4a957f890bdc87884c2bad9e76d51ad5f43609ec
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-12-08 15:06:40 -0500

    updated homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,20 @@ const router = require('express').Router();
 const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-//all html routes are doing get method is reading
-//http://localhost:3001/ ; Home page
+
+//http://localhost:3001/
 router.get('/', async (req, res) => {
   try {
+    res.render('homepage')
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http://localhost:3001/blog
+router.get('/blog', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
     const blogData = await Blog.findAll({
       include: [
         {
@@ -14,18 +24,15 @@ router.get('/', async (req, res) => {
         },
       ],
     });
-
-    const blogs = blogData.map((blog) => blog.get({ plain: true }));
-
-    res.render('homepage', {
-      blogs,
+    const blog = blogData.map((blog) => blog.get({ plain: true }));
+    res.render('blog', {
+      blog,
       logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 //http://localhost:3001/blog/1
 router.get('/blog/:id', async (req, res) => {
@@ -34,7 +41,7 @@ router.get('/blog/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['username'],
+          attributes: ['name'],
         },
       ],
     });
@@ -62,7 +69,7 @@ router.get('/profile', withAuth, async (req, res) => {
     });
 
     const user = userData.get({ plain: true });
-
+    console.log(user)
     res.render('profile', {
       ...user,
       logged_in: true
@@ -82,7 +89,6 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
-
 //http://localhost:3001/signup
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
